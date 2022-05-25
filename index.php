f9c198c65205736654b06c05ec936e099643c527
Bogdan Caleta Ivkovic <bogdan.caleta@gmail.com>
2022-05-25 22:56:27 +0200

    updejtovao sql

--- index.php
+++ index.php
@@ -31,206 +31,254 @@ if(isset($_POST['name'])){
 
 }
 ?>
-
-<!doctype html>
+<!Doctype html>
 <html>
-    <head>
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title> LuNiBo </title>
-        <link rel="stylesheet" href="/css/all.css">
-        <link rel="stylesheet" href="/css/fontawesome.css">
-        <link rel="stylesheet" href="style.css">
-        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.1/css/all.min.css">
-        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.7/css/all.css">
+	<head>
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<title> LuNiBo </title>
+		<link rel="stylesheet" href="/css/all.css">
+		<link rel="stylesheet" href="/css/fontawesome.css">
+		<link rel="stylesheet" href="style.css">
+		<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.1/css/all.min.css">
+		<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.7/css/all.css">
 
-<link rel="preconnect" href="https://fonts.googleapis.com">
-<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-<link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@1,300&display=swap" rel="stylesheet">
+		<link rel="preconnect" href="https://fonts.googleapis.com">
+		<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+		<link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@1,300&display=swap" rel="stylesheet">
 
-<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@fortawesome/fontawesome-free@5.15.4/css/fontawesome.min.css">
-    </head>
+		<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@fortawesome/fontawesome-free@5.15.4/css/fontawesome.min.css">
+
+	</head>
+
 <body>
- <section class="home">
-    <header>
-        
-               <a href="#" class="brand">LuNiBo</a>
-                <div class="menu">
-                    <div class="btn">
-                        <i class="fas fa-times close-btn"></i>
-                  </div> 
-                    <a href="#section-main">HOME</a></li>
-                    <a href="#about">ABOUT</a></li>
-                    <a href="#menu">MENU</a></li>
-                    <a href="#reserve">RESERVE</a></li>
-                    <a href="#contact">CONTACT</a></li>
-                </div>
-                    <div class="btn">
-                        <i class="fa fa-bars menu-btn"></i>
-                        
-                    </div>
-            
+<section class="home">
+	<header>
+		
+			<a href="#" class="brand">LuNiBo</a>
+				<div class="menu">
+					<div class="btn">
+						<i class="fas fa-times close-btn"></i>
+				</div> 
+					<a href="#section-main">HOME</a></li>
+					<a href="#about">ABOUT</a></li>
+					<a href="#menu">MENU</a></li>
+					<a href="#reserve">RESERVE</a></li>
+					<a href="#contact">CONTACT</a></li>
+				</div>
+					<div class="btn">
+						<i class="fa fa-bars menu-btn"></i>
+						
+					</div>
+			
 
-    </header>
+	</header>
 
-    <section id="section-main">
+	<section id="section-main">
 
-     <div class="text-box">
-        <h1>Das weltbeste Restaurant</h1>
-        <p>
-            Ja richtig gehoert wir sind das welbeste Restaurant
-        </p>
-        <a href="#reserve" class="hero-btn">Komm und ueberzeuge dich !</a>
-        
-    
-    </div>
-   </section>
-  
+	<div class="text-box">
+		<h1>Das weltbeste Restaurant</h1>
+		<p>
+			Ja richtig gehört wir sind das weltbeste Restaurant
+		</p>
+		<a href="#reserve" class="hero-btn">Komm und ueberzeuge dich !</a>
+		
+	
+	</div>
+</section>
+
 
 </section>
 
 <!-------about--------->
 
 <section id="about">
-    <h1>Ueber uns</h1> <br>
-    
-    <div class="row">
-        <div class="course-col">
-            <h3>Lukas</h3>
-            <p>hdaskljfgbsk;dfjbg;kadbowsajbeo;ighbeawso;rdhigfo;<br>;kdhaz;klfgjvhbad;e lifgtop eray;
-                lafsdhjjg;kadshf sda;IHFG ;LKS</p>
-        </div>
-        <div class="course-col">
-            <h3>Nikola</h3>
-            <p>hdaskljfgbsk;dfjbg;kadbowsajbeo;ighbeawso;rdhigfo;<br>;kdhaz;klfgjvhbad;e lifgtop eray;
-                lafsdhjjg;kadshf sda;IHFG ;LKS</p>
-        </div>
-        <div class="course-col">
-            <h3>Bogdan</h3>
-            <p>hdaskljfgbsk;dfjbg;kadbowsajbeo;ighbeawso;rdhigfo;<br>;kdhaz;klfgjvhbad;e lifgtop eray;
-                lafsdhjjg;kadshf sda;IHFG ;LKS</p>
-        </div>
-    </div>
+	<h1>Über uns</h1> <br>
+	
+	<div class="row">
+		<div class="course-col">
+			<h3>Lukas</h3>
+			<p>hdaskljfgbsk;dfjbg;kadbowsajbeo;ighbeawso;rdhigfo;<br>;kdhaz;klfgjvhbad;e lifgtop eray;
+				lafsdhjjg;kadshf sda;IHFG ;LKS</p>
+		</div>
+		<div class="course-col">
+			<h3>Nikola</h3>
+			<p>hdaskljfgbsk;dfjbg;kadbowsajbeo;ighbeawso;rdhigfo;<br>;kdhaz;klfgjvhbad;e lifgtop eray;
+				lafsdhjjg;kadshf sda;IHFG ;LKS</p>
+		</div>
+		<div class="course-col">
+			<h3>Bogdan</h3>
+			<p>hdaskljfgbsk;dfjbg;kadbowsajbeo;ighbeawso;rdhigfo;<br>;kdhaz;klfgjvhbad;e lifgtop eray;
+				lafsdhjjg;kadshf sda;IHFG ;LKS</p>
+		</div>
+	</div>
 </section>
 
 <!----- Menu ----->
 
 <section id="menu">
 
-    <h1>Unser wunderbares und leckeres Menu</h1> <br><br><br><br><br>
-    <p>Hier kommen dann die ganzen Bilder mit den Preisen</p>
+	<h1>Unser wunderbares und leckeres Menü</h1> <br><br><br><br><br>
+	<div class="container">
+		<div class="menu">
+		<h2 class="menu-group-heading">
+			Hauptspeisen
+		</h2>
+		<div class="menu-group">
+			<div class="menu-item">
+			
+			<div class="menu-item-text">
+				<h3 class="menu-item-heading">
+				<span class="menu-item-name">Bruschetta</span>
+				<span class="menu-item-price">$12.90</span>
+				</h3>
+				<p class="menu-item-description">
+				Nunc efficitur felis vel mi efficitur, sed molestie sem scelerisque. Fusce orci risus,
+				congue eu mauris nec, pretium tincidunt nulla.
+				</p>
+			</div>
+			</div>
+			<div class="menu-item">
+			
+			<div class="menu-item-text">
+				<h3 class="menu-item-heading">
+				<span class="menu-item-name">Bruschetta</span>
+				<span class="menu-item-price">$12.90</span>
+				</h3>
+				<p class="menu-item-description">
+				Nunc efficitur felis vel mi efficitur, sed molestie sem scelerisque. Fusce orci risus,
+				congue eu mauris nec, pretium tincidunt nulla.
+				</p>
+			</div>
+			</div>
+		</div>
+		<h2 class="menu-group-heading">
+			Dessert
+		</h2>
+		<div class="menu-group">
+			<div class="menu-item">
+			
+			<div class="menu-item-text">
+				<h3 class="menu-item-heading">
+				<span class="menu-item-name">Bruschetta</span>
+				<span class="menu-item-price">$12.90</span>
+				</h3>
+				<p class="menu-item-description">
+				Nunc efficitur felis vel mi efficitur, sed molestie sem scelerisque. Fusce orci risus,
+				congue eu mauris nec, pretium tincidunt nulla.
+				</p>
+			</div>
+			</div>
+			<div class="menu-item">
+			
+			<div class="menu-item-text">
+				<h3 class="menu-item-heading">
+				<span class="menu-item-name">Bruschetta</span>
+				<span class="menu-item-price">$12.90</span>
+				</h3>
+				<p class="menu-item-description">
+				Nunc efficitur felis vel mi efficitur, sed molestie sem scelerisque. Fusce orci risus,
+				congue eu mauris nec, pretium tincidunt nulla.
+				</p>
+			</div>
+			</div>
+		</div>
+		</div>
+	</div>
 
 </section>
 
 <!----- Reserve ----->
 
-   <section id="reserve" >
+<section id="reserve" >
 
-    <h1>Reserviere einen <br> Tisch</h1> <br><br><br><br>
+	<h1>Reserviere einen <br> Tisch</h1> <br><br><br><br>
 
-    <form action="index.php" method="POST">
-        <div>
-            <span>Your full name ?</span>
-            <input type="text" name="name" id="name" placeholder="Write your name here..." required>
-        </div>
-        <div>
-            <span>Your email address ?</span>
-            <input type="email" name="email" id="name" placeholder="Write your email here..." required> 
-        </div>
-        <div>
-            <!-- <---this is the select option--->
-            <span>How many people ?</span>
-            <select name="people" id="people" required>
-                <option value=""> <---People---></option>
-                <option value="1">1 People</option>
-                <option value="2">2 People</option>
-                <option value="3">3 People</option>
-                <option value="4">4 People</option>
-                <option value="4">5 People</option>
-                <option value="4">6 People</option>
-                <option value="4">7 People</option>
-                <option value="4">8 People</option>
-                <option value="4">9 People</option>
-            </select>
-            <!-- this is the select option -->
-        </div>
-        <div>
-            <span>What time ?</span>
-            <input type="time" name="time" id="time"  min="10:00" max="23:00" required>
-        </div>
-        <div>
-            <span>What is the date ?</span>
-            <input type="date" name="date" id="date" placeholder="date" required>
-        </div>
-        <div>
-            <span>Your phone number ?</span>
-            <input type="number" name="number" id="number" placeholder="Write your number here..." required>
-        </div> 
-        <br>
-        <div id="submit">
-            <input type="submit" value="SUBMIT" id="submit">
-        </div>
+	<form action="index.php" method="POST">
+		<div>
+			<span>Your full name ?</span>
+			<input type="text" name="name" id="name" placeholder="Write your name here..." required>
+		</div>
+		<div>
+			<span>Your email address ?</span>
+			<input type="email" name="email" id="name" placeholder="Write your email here..." required> 
+		</div>
+		<div>
+			<!-- <---this is the select option--->
+			<span>How many people ?</span>
+			<select name="people" id="people" required>
+				<option value=""> <---People---></option>
+				<option value="1">1 People</option>
+				<option value="2">2 People</option>
+				<option value="3">3 People</option>
+				<option value="4">4 People</option>
+				<option value="4">5 People</option>
+				<option value="4">6 People</option>
+				<option value="4">7 People</option>
+				<option value="4">8 People</option>
+				<option value="4">9 People</option>
+			</select>
+			<!-- <---this is the select option--->
+		</div>
+		<div>
+			<span>What time ?</span>
+			<input type="time" name="time" id="time"  min="10:00" max="23:00" required>
+			
+		</div>
+		<div>
+			<span>What is the date ?</span>
+			<input type="date" name="date" id="date" placeholder="date" required>
+		</div>
+		<div>
+			<span>Your phone number ?</span>
+			<input type="number" name="number" id="number" placeholder="Write your number here..." required>
+		</div> 
+		<br>
+		<div id="submit">
+			<input type="submit" value="SUBMIT" id="submit">
+		</div>
 
 
-    </form>
+	</form>
 
-   </section>
+</section>
 
-   <section id="contact">
+<section id="contact">
 
-    <!--alert messages start
-    <div class="alert-success">
-      <span>Message Sent! Thank you for contacting us.</span>
-    </div>
-    <div class="alert-error">
-      <span>Something went wrong! Please try again.</span>
-    </div>
-    alert messages end-->
+	<!--alert messages start
+	<div class="alert-success">
+	<span>Message Sent! Thank you for contacting us.</span>
+	</div>
+	<div class="alert-error">
+	<span>Something went wrong! Please try again.</span>
+	</div>
+	alert messages end-->
 
-    <!--contact section start-->
+	<!--contact section start-->
 
-    <div class="contact-section">
-        <div class="contact-info">
-          <div><i class="fas fa-map-marker-alt"></i>
-            Thaliastraße 125, 1160 Wien, Oesterreich</div>
-          <div><i class="fas fa-envelope"></i>panic.n99@htlwienwest.at</div>
-          <div><i class="fas fa-phone"></i>+43 660 253 732 33</div>
-          <div><i class="fas fa-clock"></i>Mo - SO 10:00 AM to 1:00 PM</div>
-        </div>
-        <div class="contact-form">
-          <h2>Contact Us</h2> 
-          <form class="contact" action="" method="post">
-            <input type="text" name="name1" class="text-box1" placeholder="Your Name" required>
-            <input type="email" name="email1" class="text-box1" placeholder="Your Email" required>
-            <textarea name="message" rows="5" placeholder="Your Message" required></textarea>
-            <input type="submit"  id="send-btn" value="Send">
-          </form>
-        </div>
-      </div>
-      
-      <!--contact section end-->
-    
-   </section>
+	<div class="contact-section">
+		<div class="contact-info">
+		<div><i class="fas fa-map-marker-alt"></i>
+			Thaliastraße 125, 1160 Wien, Österreich</div>
+		<div><i class="fas fa-envelope"></i>panic.n99@htlwienwest.at</div>
+		<div><i class="fas fa-phone"></i>+43 660 253 732 33</div>
+		<div><i class="fas fa-clock"></i>Mo - SO 10:00 AM to 1:00 PM</div>
+		</div>
+		<div class="contact-form">
+		<h2>Contact Us</h2> 
+		<form class="contact" action="" method="post">
+			<input type="text" name="name1" class="text-box1" placeholder="Your Name" required>
+			<input type="email" name="email1" class="text-box1" placeholder="Your Email" required>
+			<textarea name="message" rows="5" placeholder="Your Message" required></textarea>
+			<input type="submit"  id="send-btn" value="Send">
+		</form>
+		</div>
+	</div>
+	
+	<!--contact section end-->
+	
+</section>
 
-   <script type="text/javascript">
-    //javascript for navigation bar effect on scroll
-    window.addEventListener("scroll", function(){
-      var header = document.querySelector("header");
-      header.classList.toggle('sticky', window.scrollY > 0);
-    });
-
-    //javascript for responsive navigation sidebar menu
-    var menu = document.querySelector('.menu');
-    var menuBtn = document.querySelector('.menu-btn');
-    var closeBtn = document.querySelector('.close-btn');
-
-    menuBtn.addEventListener("click", () => {
-      menu.classList.add('active');
-    });
-
-    closeBtn.addEventListener("click", () => {
-      menu.classList.remove('active');
-    });
-    </script>
+<script src="main.js"></script>
 
 </body>
 
--- lunibo.sql
+++ lunibo.sql
@@ -1,195 +1,205 @@
--- phpMyAdmin SQL Dump
--- version 5.1.1
--- https://www.phpmyadmin.net/
+-- MariaDB dump 10.19  Distrib 10.4.21-MariaDB, for Win64 (AMD64)
 --
--- Host: localhost
--- Erstellungszeit: 24. Mai 2022 um 21:03
--- Server-Version: 10.4.21-MariaDB
--- PHP-Version: 8.0.12
-
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-START TRANSACTION;
-SET time_zone = "+00:00";
-
+-- Host: 192.168.0.220    Database: lunibo
+-- ------------------------------------------------------
+-- Server version	10.5.15-MariaDB-0+deb11u1
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
 /*!40101 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
--- Datenbank: `lunibo`
+-- Table structure for table `customers`
 --
 
--- --------------------------------------------------------
-
---
--- Tabellenstruktur für Tabelle `customers`
---
-
+DROP TABLE IF EXISTS `customers`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `customers` (
-  `customerID` int(11) NOT NULL,
-  `name` varchar(50) NOT NULL,
-  `lastname` varchar(50) DEFAULT NULL,
+  `customerID` int(11) NOT NULL AUTO_INCREMENT,
+  `fullname` varchar(50) NOT NULL,
   `email` varchar(50) NOT NULL,
-  `phone` varchar(20) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
--- --------------------------------------------------------
+  `phone` varchar(20) DEFAULT NULL,
+  PRIMARY KEY (`customerID`),
+  UNIQUE KEY `customers_customerID_uindex` (`customerID`)
+) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=utf8mb4;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Tabellenstruktur für Tabelle `menu`
+-- Dumping data for table `customers`
 --
 
+LOCK TABLES `customers` WRITE;
+/*!40000 ALTER TABLE `customers` DISABLE KEYS */;
+INSERT INTO `customers` VALUES (1,'Jane','jane.doe@gmail.com','0676 812 82 53'),(2,'Barbara Goble','BarbaraDGoble@cuvox.de','0676 740 57 42'),(3,'Michael Frei','MichaelFrei@cuvox.de','0676 410 00 64'),(4,'Marcel Gruenwald','MarcelGruenewald@einrot.com','0650 780 13 13'),(5,'Doreen Roth','DoreenRoth@cuvox.de','0650 432 89 56'),(6,'Peter Theissen','PeterTheissen@cuvox.de','0664 748 32 64'),(11,'Bogdan','bogdan.caleta@gmail.com','+4368181790350'),(12,'Bogdan Caleta','bogdan.caleta@gmail.com','068181790350');
+/*!40000 ALTER TABLE `customers` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Temporary table structure for view `free_tables`
+--
+
+DROP TABLE IF EXISTS `free_tables`;
+/*!50001 DROP VIEW IF EXISTS `free_tables`*/;
+SET @saved_cs_client     = @@character_set_client;
+SET character_set_client = utf8;
+/*!50001 CREATE TABLE `free_tables` (
+  `tableID` tinyint NOT NULL,
+  `maxPeople` tinyint NOT NULL
+) ENGINE=MyISAM */;
+SET character_set_client = @saved_cs_client;
+
+--
+-- Table structure for table `menu`
+--
+
+DROP TABLE IF EXISTS `menu`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `menu` (
-  `itemID` int(11) NOT NULL,
+  `itemID` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(30) NOT NULL,
   `description` text NOT NULL,
   `price` decimal(2,0) NOT NULL,
   `listOrder` int(11) NOT NULL,
-  `kcal` int(11) DEFAULT NULL
+  `kcal` int(11) DEFAULT NULL,
+  PRIMARY KEY (`itemID`),
+  UNIQUE KEY `menu_itemID_uindex` (`itemID`),
+  UNIQUE KEY `menu_name_uindex` (`name`),
+  UNIQUE KEY `menu_listOrder_uindex` (`listOrder`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
--- --------------------------------------------------------
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Tabellenstruktur für Tabelle `reservations`
+-- Dumping data for table `menu`
 --
 
+LOCK TABLES `menu` WRITE;
+/*!40000 ALTER TABLE `menu` DISABLE KEYS */;
+/*!40000 ALTER TABLE `menu` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `reservations`
+--
+
+DROP TABLE IF EXISTS `reservations`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `reservations` (
-  `resID` int(11) NOT NULL,
+  `resID` int(11) NOT NULL AUTO_INCREMENT,
   `reservationDatetime` datetime NOT NULL,
   `numOfPeople` int(11) NOT NULL,
-  `tableID` int(11) NOT NULL,
+  `tableID` int(11) DEFAULT NULL,
   `customerID` int(11) NOT NULL,
-  `deleted` tinyint(1) NOT NULL DEFAULT 0
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
--- --------------------------------------------------------
+  `deleted` tinyint(1) NOT NULL DEFAULT 0,
+  PRIMARY KEY (`resID`),
+  UNIQUE KEY `reservations_resID_uindex` (`resID`),
+  KEY `reservations_tables_tableID_fk` (`tableID`),
+  KEY `reservations_customers_customerID_fk` (`customerID`),
+  CONSTRAINT `reservations_customers_customerID_fk` FOREIGN KEY (`customerID`) REFERENCES `customers` (`customerID`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `reservations_tables_tableID_fk` FOREIGN KEY (`tableID`) REFERENCES `tables` (`tableID`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=utf8mb4;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Tabellenstruktur für Tabelle `tables`
+-- Dumping data for table `reservations`
 --
 
+LOCK TABLES `reservations` WRITE;
+/*!40000 ALTER TABLE `reservations` DISABLE KEYS */;
+INSERT INTO `reservations` VALUES (3,'2022-06-25 16:07:37',5,10,4,0),(4,'2022-06-25 16:07:44',2,3,2,0),(5,'2022-06-25 16:07:47',2,4,3,0),(6,'2022-03-28 16:07:59',4,6,1,0),(7,'2022-02-03 12:40:00',3,7,5,0),(8,'2022-05-25 16:07:51',6,12,6,0),(9,'2022-02-07 12:00:00',10,18,4,0),(10,'2022-02-06 21:00:00',3,5,11,0),(11,'2022-08-05 21:45:00',4,8,12,0);
+/*!40000 ALTER TABLE `reservations` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `tables`
+--
+
+DROP TABLE IF EXISTS `tables`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `tables` (
-  `tableID` int(11) NOT NULL,
-  `maxPeople` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
--- --------------------------------------------------------
+  `tableID` int(11) NOT NULL AUTO_INCREMENT,
+  `maxPeople` int(11) NOT NULL,
+  PRIMARY KEY (`tableID`),
+  UNIQUE KEY `Tables_tableID_uindex` (`tableID`)
+) ENGINE=InnoDB AUTO_INCREMENT=19 DEFAULT CHARSET=utf8mb4;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Tabellenstruktur für Tabelle `users`
+-- Dumping data for table `tables`
 --
 
+LOCK TABLES `tables` WRITE;
+/*!40000 ALTER TABLE `tables` DISABLE KEYS */;
+INSERT INTO `tables` VALUES (1,2),(2,2),(3,2),(4,2),(5,4),(6,4),(7,4),(8,4),(9,4),(10,6),(11,6),(12,6),(13,6),(14,8),(15,8),(16,8),(17,10),(18,10);
+/*!40000 ALTER TABLE `tables` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `users`
+--
+
+DROP TABLE IF EXISTS `users`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `users` (
-  `userID` int(11) NOT NULL,
+  `userID` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(50) NOT NULL,
   `lastname` varchar(50) NOT NULL,
   `email` varchar(50) NOT NULL,
   `password` varchar(50) NOT NULL,
-  `deleted` tinyint(1) NOT NULL DEFAULT 0
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+  `deleted` tinyint(1) NOT NULL DEFAULT 0,
+  PRIMARY KEY (`userID`),
+  UNIQUE KEY `users_email_uindex` (`email`),
+  UNIQUE KEY `users_userID_uindex` (`userID`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Daten für Tabelle `users`
+-- Dumping data for table `users`
 --
 
-INSERT INTO `users` (`userID`, `name`, `lastname`, `email`, `password`, `deleted`) VALUES
-(1, 'bogdan', 'caleta ivkovic', 'bogdan.caleta@gmail.com', '5f4dcc3b5aa765d61d8327deb882cf99', 0),
-(2, 'nikola', 'panic', 'n.panic@gmx.at', '5f4dcc3b5aa765d61d8327deb882cf99', 0),
-(3, 'lukas', 'zankel', 'zankel.l88@htlwienwest.at', '5f4dcc3b5aa765d61d8327deb882cf99', 0);
+LOCK TABLES `users` WRITE;
+/*!40000 ALTER TABLE `users` DISABLE KEYS */;
+INSERT INTO `users` VALUES (1,'bogdan','caleta ivkovic','bogdan.caleta@gmail.com','5f4dcc3b5aa765d61d8327deb882cf99',0),(2,'nikola','panic','n.panic@gmx.at','5f4dcc3b5aa765d61d8327deb882cf99',0),(3,'lukas','zankel','zankel.l88@htlwienwest.at','5f4dcc3b5aa765d61d8327deb882cf99',0);
+/*!40000 ALTER TABLE `users` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
--- Indizes der exportierten Tabellen
+-- Final view structure for view `free_tables`
 --
 
---
--- Indizes für die Tabelle `customers`
---
-ALTER TABLE `customers`
-  ADD PRIMARY KEY (`customerID`),
-  ADD UNIQUE KEY `customers_customerID_uindex` (`customerID`);
+/*!50001 DROP TABLE IF EXISTS `free_tables`*/;
+/*!50001 DROP VIEW IF EXISTS `free_tables`*/;
+/*!50001 SET @saved_cs_client          = @@character_set_client */;
+/*!50001 SET @saved_cs_results         = @@character_set_results */;
+/*!50001 SET @saved_col_connection     = @@collation_connection */;
+/*!50001 SET character_set_client      = utf8mb4 */;
+/*!50001 SET character_set_results     = utf8mb4 */;
+/*!50001 SET collation_connection      = utf8mb4_general_ci */;
+/*!50001 CREATE ALGORITHM=UNDEFINED */
+/*!50013 DEFINER=`pi`@`192.168.%.%` SQL SECURITY DEFINER */
+/*!50001 VIEW `free_tables` AS select `tables`.`tableID` AS `tableID`,`tables`.`maxPeople` AS `maxPeople` from `tables` where !(`tables`.`tableID` in (select `reservations`.`tableID` from `reservations` where `reservations`.`deleted` = 0 and `reservations`.`reservationDatetime` < current_timestamp())) */;
+/*!50001 SET character_set_client      = @saved_cs_client */;
+/*!50001 SET character_set_results     = @saved_cs_results */;
+/*!50001 SET collation_connection      = @saved_col_connection */;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
---
--- Indizes für die Tabelle `menu`
---
-ALTER TABLE `menu`
-  ADD PRIMARY KEY (`itemID`),
-  ADD UNIQUE KEY `menu_itemID_uindex` (`itemID`),
-  ADD UNIQUE KEY `menu_name_uindex` (`name`),
-  ADD UNIQUE KEY `menu_listOrder_uindex` (`listOrder`);
-
---
--- Indizes für die Tabelle `reservations`
---
-ALTER TABLE `reservations`
-  ADD PRIMARY KEY (`resID`),
-  ADD UNIQUE KEY `reservations_resID_uindex` (`resID`),
-  ADD KEY `reservations_tables_tableID_fk` (`tableID`),
-  ADD KEY `reservations_customers_customerID_fk` (`customerID`);
-
---
--- Indizes für die Tabelle `tables`
---
-ALTER TABLE `tables`
-  ADD PRIMARY KEY (`tableID`),
-  ADD UNIQUE KEY `Tables_tableID_uindex` (`tableID`);
-
---
--- Indizes für die Tabelle `users`
---
-ALTER TABLE `users`
-  ADD PRIMARY KEY (`userID`),
-  ADD UNIQUE KEY `users_email_uindex` (`email`),
-  ADD UNIQUE KEY `users_userID_uindex` (`userID`);
-
---
--- AUTO_INCREMENT für exportierte Tabellen
---
-
---
--- AUTO_INCREMENT für Tabelle `customers`
---
-ALTER TABLE `customers`
-  MODIFY `customerID` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT für Tabelle `menu`
---
-ALTER TABLE `menu`
-  MODIFY `itemID` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT für Tabelle `reservations`
---
-ALTER TABLE `reservations`
-  MODIFY `resID` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT für Tabelle `tables`
---
-ALTER TABLE `tables`
-  MODIFY `tableID` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT für Tabelle `users`
---
-ALTER TABLE `users`
-  MODIFY `userID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
-
---
--- Constraints der exportierten Tabellen
---
-
---
--- Constraints der Tabelle `reservations`
---
-ALTER TABLE `reservations`
-  ADD CONSTRAINT `reservations_customers_customerID_fk` FOREIGN KEY (`customerID`) REFERENCES `customers` (`customerID`) ON DELETE CASCADE ON UPDATE CASCADE,
-  ADD CONSTRAINT `reservations_tables_tableID_fk` FOREIGN KEY (`tableID`) REFERENCES `tables` (`tableID`) ON DELETE CASCADE ON UPDATE CASCADE;
-COMMIT;
-
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2022-05-25 22:55:35
